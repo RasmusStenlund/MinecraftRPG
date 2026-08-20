9c12c47cf46b0a43f21679e8330e64fb1bc01570
RasmusStenlund <rasmus.stenlund@gmail.com>
2026-08-20 22:49:04 +0200

    Add classes for components in game and add basic labels for player stats

--- MinecraftRPG/MinecraftRPG.cs
+++ MinecraftRPG/MinecraftRPG.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,22 @@ namespace MinecraftRPG
 {
     public partial class MinecraftRPG : Form
     {
+        private Player _player;
         public MinecraftRPG()
         {
             InitializeComponent();
+
+            _player = new Player();
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Emeralds = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblEmeralds.Text = _player.Emeralds.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
         }
     }
 }
